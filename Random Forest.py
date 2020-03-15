4d4c4be48989802e2814734001430af0740d3c1f
Gent <gent.bajko@gmail.com>
2020-03-15 19:53:26 +0100

    Version 0.9

---
+++ Random Forest.py
@@ -0,0 +1,22 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.model_selection import train_test_split as tts
+import pandas as pd
+import os
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+dataset = pd.read_excel(os.path.join(__location__, 'data.xlsx'), sheet_name='Dataset')
+
+X = dataset.drop('Result', axis=1)
+y = dataset['Result']
+
+X_train, X_test, y_train, y_test = tts(X, y, random_state=1, test_size = 0.20)
+
+random_forest = RandomForestClassifier(n_estimators=100, max_depth=100, random_state=1000)
+random_forest.fit(X_train, y_train)
+
+y_pred = random_forest.predict(X_test)
+accuracy_score(y_test, y_pred)
+
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
--- svm.py
+++ svm.py
@@ -1,6 +1,23 @@
-import numpy as np
 import pandas as pd
-import statsmodels.api as sm
-from sklearn.model_selection import MinMaxScaler
 from sklearn.model_selection import train_test_split as tts
-from sklearn.utils import shuffle
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, confusion_matrix
+import os
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+dataset = pd.read_excel(os.path.join(__location__, 'data.xlsx'), sheet_name='Dataset')
+
+dataset = dataset.tail(2000)
+
+X = dataset.drop('Result', axis=1)
+y = dataset['Result']
+
+X_train, X_test, y_train, y_test = tts(X, y, test_size = 0.20)
+
+svclassifier = SVC(kernel='linear', C=1.0)
+svclassifier.fit(X_train, y_train)
+
+y_pred = svclassifier.predict(X_test)
+
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
